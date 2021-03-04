715818c3f57715883e5507a53828c561cb970f18
Rebecca <graham.cali@gmail.com>
2021-03-03 23:19:11 -0600

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cocktail.create(name: "Blood-Orange Margarita", image_url: "https://cookieandkate.com/images/2014/01/blood-orange-margaritas.jpg")
+Cocktail.create(name: "Espresso Martini", image_url: "https://www.thespruceeats.com/thmb/KuL-FReVfIQ8HfNFv-zfWkV8LhE=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/espresso-martini-recipe-761050-5-5b490046c9e77c0037b55421.jpg")
+Cocktail.create(name: "Mojito", image_url: "https://www.gimmesomeoven.com/wp-content/uploads/2018/05/How-To-Make-A-Mojito-Recipe-Cocktail-Fresh-Honey-3-1100x1651.jpg")
