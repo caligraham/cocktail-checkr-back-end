1ba63aa056e852a8780f1ce6f0fff71cc308e823
Rebecca <graham.cali@gmail.com>
2021-03-03 23:18:21 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  
+  resources :cocktails, only: [:index] do
+    resources :reviews, only: [:create, :index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
