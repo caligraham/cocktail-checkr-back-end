c085875d762b904163a4bc1700618dd1a0c3d0bf
Rebecca <graham.cali@gmail.com>
2021-03-03 23:46:39 -0600

    reviews create route

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,17 @@
 class ReviewsController < ApplicationController
+
+    def create
+        cocktail = Cocktail.find_by(id: params[:cocktail_id])
+        review = cocktail.reviews.build(review_params)
+        if review.save
+            render json: review
+        end    
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:title, :content, :rating)
+    end
+    
 end
