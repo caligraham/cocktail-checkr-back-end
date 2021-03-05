4cd599f9dc49ad2949fc5c807c7554a2e59eb455
Rebecca <graham.cali@gmail.com>
2021-03-05 17:32:34 -0600

    rebugging seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,4 @@ Cocktail.create(name: "Blood-Orange Margarita", image_url: "https://cookieandkat
 Cocktail.create(name: "Espresso Martini", image_url: "https://www.thespruceeats.com/thmb/KuL-FReVfIQ8HfNFv-zfWkV8LhE=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/espresso-martini-recipe-761050-5-5b490046c9e77c0037b55421.jpg")
 Cocktail.create(name: "Mojito", image_url: "https://www.gimmesomeoven.com/wp-content/uploads/2018/05/How-To-Make-A-Mojito-Recipe-Cocktail-Fresh-Honey-3-1100x1651.jpg")
 
-Review.create(title: "Fantastic", content: "So delicious and refreshing. I could drink 10", rating: 5, cocktail_id: 1)
+Review.create(title: "Fantastic", content: "So delicious and refreshing. I could drink 10", rating: 5)
