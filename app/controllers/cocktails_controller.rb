e97cfc1efbc2356d0619f60791d5171f7a2ce4d5
Rebecca <graham.cali@gmail.com>
2021-03-25 16:00:24 -0500

    last minute fix

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -4,5 +4,10 @@ class CocktailsController < ApplicationController
         cocktails = Cocktail.all
         render json: cocktails
     end
+
+    def show
+        cocktail = Cocktail.find_by(id: params[:id])
+        render json: cocktail, :include => [:reviews]
+    end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  resources :cocktails, only: [:index] do
+  resources :cocktails, only: [:index, :show] do
     resources :reviews, only: [:create, :index]
   end
   
