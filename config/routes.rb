51308bb56c9777a6ff0dde9ba682ae67df2a9e84
Rebecca <graham.cali@gmail.com>
2021-03-25 13:36:31 -0500

    removed default comment

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :cocktails, only: [:index] do
     resources :reviews, only: [:create, :index]
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
